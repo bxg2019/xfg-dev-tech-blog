1c09a6ee373e372c9761948131d0a39aeabedcb2
bxg2019 <1944639950@qq.com>
2024-07-12 13:13:25 +0800

    docs: update blog

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -2,7 +2,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  dest: "./nginx/html",
+  dest: "./dev-ops/nginx/html",
   base: "/",
 
   lang: "zh-CN",
